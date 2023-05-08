1e74e838ed41d19d24f6168dbfa0f746fc0e22c6
dasiklasik <daryasamsonowicz@gmail.com>
2023-05-08 10:35:30 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
 
+(function copyDir (){
+  const originalPath = path.resolve(__dirname, 'files/');
+  const newPath =  path.resolve(__dirname, 'files-copy/');
+
+  fsPromises.mkdir(newPath, {recursive: true})
+    .then(() => {
+
+      return fsPromises.readdir(newPath, {withFileTypes: true});
+    })
+    .then(data => {
+      for (let item of data) {
+        fsPromises.unlink(path.resolve(newPath, item.name))
+          .catch(err => {
+            if (err) console.log(err);
+          });
+      }
+      return fsPromises.readdir(originalPath, {withFileTypes: true});
+    })
+    .then((data) => {
+      for (let item of data) {
+        const filePath = path.resolve(originalPath, item.name);
+        const newFilePath = path.resolve(newPath, item.name);
+        fsPromises.copyFile(filePath, newFilePath)
+          .catch(err => {
+            if (err) console.log(err);
+          });
+      }
+    });
+})();
